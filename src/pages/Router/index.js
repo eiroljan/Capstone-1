b07b57ddc7863c9ee448df43f1814985d5ed1ee5
jeffigy <evangelistajeffy25@gmail.com>
2021-11-15 23:41:53 +0800

    add stuff

--- src/pages/Router/index.js
+++ src/pages/Router/index.js
@@ -8,11 +8,12 @@ import Profile from 'pages/Profile';
 import ResetPassword from 'pages/ResetPassword';
 import NotFound from 'pages/NotFound';
 import User from 'pages/User';
-import Section from 'pages/Section';
+import Clients from 'pages/Clients';
 import Submenu from 'pages/Submenu';
 import Teams from 'pages/Teams';
 import paths from './paths';
 import PrivateRoute from './PrivateRoute';
+// import Client from 'pages/Client';
 
 const RouterComponent = () => {
   return (
@@ -24,12 +25,14 @@ const RouterComponent = () => {
         <PrivateRoute path={paths.MODIFY_USER} component={User} />
         <PrivateRoute path={paths.USERS} component={Users} />
         <PrivateRoute path={paths.PROFILE} component={Profile} />
-        <PrivateRoute path={paths.SECTION} component={Section} />
+        <PrivateRoute path={paths.CLIENTS} component={Clients} />
         <PrivateRoute path={paths.SUBMENU_1} component={Submenu} />
         <PrivateRoute path={paths.SUBMENU_2} component={Submenu} />
         <PrivateRoute path={paths.TEAMS} component={Teams} />
         <PrivateRoute path={paths.ROOT} component={Home} />
-        <PrivateRoute path={paths.SECTION} component={Section} />
+        <PrivateRoute path={paths.CLIENTS} component={Clients} />
+        {/* <PrivateRoute path={paths.ADD_CLIENT} component={Client} /> */}
+        
         <Route component={NotFound} />
       </Switch>
     </BrowserRouter>
